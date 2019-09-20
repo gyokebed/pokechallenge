175b1dfff0291414ace431d659a01eae24e30b18
Gustavo Yokebed <g.yokebed@gmail.com>
2019-09-19 20:23:48 -0500

    Displayed name and image from pokemon API

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,8 +3,13 @@ import React from "react";
 export const CardList = props => {
   console.log(props);
   return (
-    <div>
-      <h1>CardList</h1>
+    <div className="card-list">
+      {props.pokemon.map(pokemon => (
+        <div className="card-container" key={pokemon.id}>
+          <h1>{pokemon.name}</h1>
+          <img alt="pokemon" src={pokemon.imageUrl} />
+        </div>
+      ))}
     </div>
   );
 };
