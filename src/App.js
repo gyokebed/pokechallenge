f244e91044cdd6ee02a24afbac624af68d1920f4
Gustavo Yokebed <g.yokebed@gmail.com>
2019-09-20 17:39:31 -0500

    Added pagination to pokemon cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import { Pagination } from "./components/common/pagination.component";
+
 import logo from "./logo.svg";
 import axios from "axios";
 
@@ -9,26 +11,55 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      cards: []
+      cards: [],
+      currentPage: 1
     };
   }
 
   componentDidMount() {
     const getCards = async () => {
       const response = await axios.get("https://api.pokemontcg.io/v1/cards");
-      console.log(response);
-      this.setState({ cards: response.data.cards });
+      console.log(response.headers);
+      this.setState({
+        cards: response.data.cards,
+        pageSize: response.headers["page-size"],
+        totalCards: response.headers["total-count"]
+      });
     };
 
     getCards();
   }
 
+  handlePageChange = page => {
+    console.log(page);
+
+    const getCards = async () => {
+      const response = await axios.get(
+        `https://api.pokemontcg.io/v1/cards?page=${page}`
+      );
+      console.log(response.headers);
+      this.setState({
+        cards: response.data.cards,
+        pageSize: response.headers["page-size"],
+        currentPage: page
+      });
+    };
+
+    getCards();
+  };
+
   render() {
-    // console.log(this.state);
-    const { cards } = this.state;
+    console.log(this.state);
+    const { cards, currentPage, pageSize, totalCards } = this.state;
     return (
       <div className="App">
         <CardList pokemon={cards} />
+        <Pagination
+          itemsCount={totalCards}
+          currentPage={currentPage}
+          pageSize={pageSize}
+          onPageChange={this.handlePageChange}
+        ></Pagination>
       </div>
     );
   }
---
+++ src/components/common/pagination.component.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import _ from "lodash";
+import "./pagination.styles.scss";
+
+export const Pagination = ({
+  currentPage,
+  pageSize,
+  itemsCount,
+  onPageChange
+}) => {
+  const pagesCount = Math.ceil(itemsCount / pageSize);
+  //console.log(pagesCount);
+  if (pagesCount === 1) return null;
+  const pages = _.range(1, pagesCount + 1);
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pages.map(page => (
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a className="page-link" onClick={() => onPageChange(page)}>
+              {page}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
