aa4d0997d2d552d57760bda336df661a77f20671
Gustavo Carrillo (Laptop) <g.yokebed@gmail.com>
2019-09-23 11:10:06 -0500

    Fixed navbar links

--- src/components/common/nav-bar.component.jsx
+++ src/components/common/nav-bar.component.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import './common.styles.scss';
 
 const NavBar = () => {
   return (
     <div className='navbar'>
-      <NavLink to='/'>Pokemon Cards</NavLink>
-      <NavLink to='/sets'>Sets</NavLink>
+      <a href='/'>Pokemon Cards</a>
+      <a href='/sets'>Sets</a>
     </div>
   );
 }
