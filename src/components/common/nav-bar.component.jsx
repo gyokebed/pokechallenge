73df4e4a4d4c3e9914e70ef83704a81b7182a546
Gustavo Yokebed <g.yokebed@gmail.com>
2019-09-24 18:18:07 -0500

    Detect active button for navigation bar

--- src/components/common/nav-bar.component.jsx
+++ src/components/common/nav-bar.component.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Link } from "react-router-dom";
+import { withRouter, Link } from "react-router-dom";
 import { fetchCards, fetchSets } from "../../redux/actions";
 import "./common.styles.scss";
 
 const NavBar = props => {
+  console.log(props.location.pathname);
+  const pathname = props.location.pathname;
   return (
     <div className="navbar">
-      <Link
-        to="/pokemon"
-        onClick={() =>
-          props.fetchCards("https://api.pokemontcg.io/v1/cards", 1)
-        }
+      <div
+        className={pathname === "/pokemon" ? "nav-button active" : "nav-button"}
       >
-        Pokemon Cards
-      </Link>
-      <Link
-        to="/sets"
-        onClick={() => props.fetchSets("https://api.pokemontcg.io/v1/sets", 1)}
+        <Link
+          to="/pokemon"
+          onClick={() =>
+            props.fetchCards("https://api.pokemontcg.io/v1/cards", 1)
+          }
+        >
+          Pokemon Cards
+        </Link>
+      </div>
+      <div
+        className={pathname === "/sets" ? "nav-button active" : "nav-button"}
       >
-        Sets
-      </Link>
+        <Link
+          to="/sets"
+          onClick={() =>
+            props.fetchSets("https://api.pokemontcg.io/v1/sets", 1)
+          }
+        >
+          Sets
+        </Link>
+      </div>
     </div>
   );
 };
@@ -30,7 +42,9 @@ const mapDispatchToProps = {
   fetchSets
 };
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(NavBar);
+export default withRouter(
+  connect(
+    null,
+    mapDispatchToProps
+  )(NavBar)
+);
