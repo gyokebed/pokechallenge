e226a61413edea0a988cdc4ad8ab113edaa8a9d6
Gustavo Yokebed <g.yokebed@gmail.com>
2019-09-24 16:19:00 -0500

    Added Link to navbar buttons

--- src/components/common/nav-bar.component.jsx
+++ src/components/common/nav-bar.component.jsx
@@ -1,13 +1,36 @@
 import React from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import { fetchCards, fetchSets } from "../../redux/actions";
 import "./common.styles.scss";
 
-const NavBar = () => {
+const NavBar = props => {
   return (
     <div className="navbar">
-      <a href="/">Pokemon Cards</a>
-      <a href="/sets">Sets</a>
+      <Link
+        to="/pokemon"
+        onClick={() =>
+          props.fetchCards("https://api.pokemontcg.io/v1/cards", 1)
+        }
+      >
+        Pokemon Cards
+      </Link>
+      <Link
+        to="/sets"
+        onClick={() => props.fetchSets("https://api.pokemontcg.io/v1/sets", 1)}
+      >
+        Sets
+      </Link>
     </div>
   );
 };
 
-export default NavBar;
+const mapDispatchToProps = {
+  fetchCards,
+  fetchSets
+};
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(NavBar);
